534a51728766424fae8ee17d0854597c4964835c
Petushok Ilya <73244258+IlyaPetushok@users.noreply.github.com>
2022-12-28 21:49:22 +0300

    Update exercise18.sql
    change

--- aggregates/exercise18.sql
+++ aggregates/exercise18.sql
@@ -1,7 +1,8 @@
 Insert into cd.bookings(facid,memid,starttime,slots)  values (14,19,'2011-02-07',2);
 insert into cd.members (surname, firstname, address, zipcode, telephone, recommendedby, joindate) 
 values ('Guest', 'Guest', '9 Tunia Drive, Boston', 32678, '(811) 433-2527', 17, '2016-03-15');
-Select firstname, surname,((sum(bk.slots)+10)/20)*10 as hours,
-rank() over (order by ((sum(bk.slots)+10)/20)*10 desc) as rank
-from cd.bookings as bk inner join cd.members as mems on bk.memid = mems.memid
-group by mems.memid order by rank, surname, firstname;   
+select firstname, surname, hours, rank() over (order by hours desc) from
+(select firstname, surname,round(sum(bks.slots)/20.0)*10 as hours
+	from cd.bookings bks inner join cd.members mems
+	on bks.memid = mems.memid group by mems.memid
+) as subq order by rank, surname, firstname;  
