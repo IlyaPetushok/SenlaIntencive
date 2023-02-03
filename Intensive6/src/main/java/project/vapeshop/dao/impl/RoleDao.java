dcc0eefc550dace1647406141ab42c109d616a50
Petushok Ilya <a3310010752@mail.ru>
2023-02-03 18:23:42 +0300

    add int6

---
+++ Intensive6/src/main/java/project/vapeshop/dao/impl/RoleDao.java
@@ -0,0 +1,70 @@
+package project.vapeshop.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import project.vapeshop.dao.Dao;
+import project.vapeshop.entity.user.Role;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class RoleDao implements Dao<Role> {
+    private static final List<Role> roles = new ArrayList<>();
+
+    @Override
+    public boolean insertObject(Role role) {
+        role.setId(roles.size());
+        roles.add(role);
+        return true;
+    }
+
+    @Override
+    public boolean insertObjects(List<Role> t) {
+        for (Role role : t) {
+            role.setId(roles.size());
+            roles.add(role);
+        }
+        return true;
+    }
+
+    @Override
+    public List<Role> selectObjects() {
+        return roles;
+    }
+
+    @Override
+    public Role selectObject(int id) {
+        return roles.stream()
+                .filter(role -> role.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public Role update(Role role) {
+        Role role1 = roles.stream()
+                .filter(r -> r.getId() == role.getId())
+                .findAny()
+                .orElse(null);
+        if (role1 != null) {
+            delete(role1.getId());
+            roles.add(role);
+            return role;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Integer i = roles.stream()
+                .filter(role -> Objects.equals(role.getId(), id))
+                .map(role -> role.getId()).findFirst()
+                .orElse(null);
+        if (i != null) {
+            roles.remove(i.intValue());
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Intensive6/src/main/java/project/vapeshop/service/common/OrderService.java
@@ -0,0 +1,52 @@
+package project.vapeshop.service.common;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import project.vapeshop.dao.Dao;
+import project.vapeshop.dto.common.OrderDTOForBasket;
+import project.vapeshop.dto.common.OrderDTOFullInfo;
+import project.vapeshop.entity.common.Order;
+
+
+import java.util.List;
+
+@Component
+public class OrderService {
+    Dao<Order> dao;
+    ModelMapper modelMapper;
+
+    @Autowired
+    public OrderService(Dao<Order> dao, ModelMapper modelMapper) {
+        this.dao = dao;
+        this.modelMapper = modelMapper;
+    }
+
+    public OrderDTOForBasket showObject(int id) {
+        return modelMapper.map(dao.selectObject(id),OrderDTOForBasket.class);
+    }
+
+    public List<OrderDTOForBasket> showObjects() {
+        return dao.selectObjects().stream()
+                .map(order -> modelMapper.map(order,OrderDTOForBasket.class))
+                .toList();
+    }
+
+    public boolean addObject(OrderDTOFullInfo orderDTOFullInfo) {
+        return dao.insertObject(modelMapper.map(orderDTOFullInfo,Order.class));
+    }
+
+    public boolean addObjects(List<OrderDTOFullInfo> orderDTOFullInfos) {
+        return dao.insertObjects(orderDTOFullInfos.stream()
+                .map(orderDTOFullInfo -> modelMapper.map(orderDTOFullInfo,Order.class))
+                .toList());
+    }
+
+    public boolean deleteObject(int id) {
+        return dao.delete(id);
+    }
+
+    public OrderDTOForBasket updateObject(OrderDTOFullInfo orderDTOFullInfo) {
+        return modelMapper.map(dao.update(modelMapper.map(orderDTOFullInfo,Order.class)),OrderDTOForBasket.class);
+    }
+}
