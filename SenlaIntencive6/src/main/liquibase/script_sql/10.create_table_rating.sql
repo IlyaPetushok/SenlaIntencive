af3095e4467b28e58ab10b8554cd89ae6c3e4e0f
Petushok Ilya <a3310010752@mail.ru>
2023-02-10 19:33:39 +0300

    delete useless directory

--- SenlaIntencive6/src/main/liquibase/script_sql/10.create_table_rating.sql
+++
@@ -1,7 +0,0 @@
-create table rating(
-	id_rating integer primary key generated always as identity,
-	comment text default('Комментарий отсутствует'),
-	quantity_stars integer not null,
-	rating_id_item integer references item(id_item),
-	rating_id_user integer references users(id_user)
-);
